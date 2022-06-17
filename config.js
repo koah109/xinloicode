f2bbe7edc6a82f59f2ce235b185046ae820ba29a
Nguyễn Đăng Khoa <neverfall109@gmail.com>
2022-06-17 20:33:20 +0700

    Release xinloicode

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Diễm à!',
+    introDesc: `Diễm đừng lạnh lùng với Khoa nữa mà :((`,
+    btnIntro: 'hihi',
+    title: 'Diễm giận đúng không :((',
+    desc: '',
+    btnYes: 'Đúng rồi :)))',
+    btnNo: 'Khôngg nha :))',
+    question:'Sao giận nói nghe thử :((',
+    btnReply: 'Send',
+    reply: 'Ghéc',
+    mess: 'Biết mà :((((',
+    messDesc: '',
+    btnAccept: ' <3',
+    messLink: 'https://www.facebook.com/messages/t/100012582878440/' 
+}
